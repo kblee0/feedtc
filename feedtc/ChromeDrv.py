be96e414ce3c953bc29dabb99e24f9c30bc4156b
kblee0 <kblee0@gmail.com>
2025-08-05 21:08:04 +0900

    update

--- feedtc/ChromeDrv.py
+++ feedtc/ChromeDrv.py
@@ -1,7 +1,6 @@
 import logging
 
-from pyvirtualdisplay import Display
-from selenium import webdriver
+import undetected_chromedriver as uc
 
 class ChromeDrv:
     __instance = None
@@ -14,19 +13,29 @@ class ChromeDrv:
     def __init__(self):
         if self.__initialized: return
         self.__initialized = True
-        self.display = Display(visible=False, size=(1920, 1080))
-        self.display.start()
         self.driver = None
         self.current_url = None
 
     def __del__(self):
         self.quit()
-        self.display.stop()
 
     # chrome start
     def start(self):
         try:
-            if self.driver is None: self.driver = webdriver.Chrome()
+            if self.driver is not None: return
+            # Chrome 옵션 설정
+            options = uc.ChromeOptions()
+
+            # Headless "new" 모드 (중요)
+            options.add_argument('--headless=new')
+
+            # 기타 탐지 우회 옵션들
+            options.add_argument('--disable-blink-features=AutomationControlled')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+
+            # Chrome 실행
+            self.driver = uc.Chrome(options=options)
         except Exception as ex:
             logging.error("ChromeDrv failed to start.", ex)
             exit(1)
@@ -34,15 +43,18 @@ class ChromeDrv:
     # chrome quit
     def quit(self):
         if self.driver is None: return
-        self.driver.quit()
+        self.driver.service.stop()
+        try:
+            self.driver.quit()
+        except OSError:
+            pass
         self.driver = None
 
     # get url and return page
     def get(self, url):
-        self.start()
         try:
+            self.start()
             self.driver.get(url)
-            self.driver.implicitly_wait(3)
             res = self.driver.page_source
             return { "url": self.driver.current_url, "body": res }
         except Exception as ex:
--- feedtc/FeedTc.py
+++ feedtc/FeedTc.py
@@ -40,6 +40,8 @@ class FeedTc:
             with open(self.config_file, 'w', encoding='utf8') as stream:
                 yaml.safe_dump(self.config, stream, allow_unicode=True, sort_keys=False)
 
+        ChromeDrv().quit()
+
 ##########################################################
 # FeedTcTask
 ##########################################################
